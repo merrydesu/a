3e83f3eaf9dbfad0e17da54ab20bf74e5232d7fb
merrydesu <115076238+merrydesu@users.noreply.github.com>
2022-10-08 19:01:40 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,1 @@
+music.magicWand.play()
---
+++ tilemap.g.ts
@@ -0,0 +1,15 @@
+// 自動生成されたコードです。変更しないでください。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// 自動生成されたコードです。変更しないでください。
